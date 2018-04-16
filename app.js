c6e022864cb01c39dc05a20923113d2b20423db1
TRAN.VICTOR.CHRISTOPHER.1541469393 <victor.c.tran2.ctr@mail.mil>
2018-04-16 09:54:02 -0400

    first commit

--- app.js
+++ app.js
@@ -1,38 +1,59 @@
-var myDogApp = angular.module("myDogApp", ["dragularModule"]);
+var myDogApp = angular.module("myDogApp", []);
 
 
 
-myDogApp.controller('DogController',['$scope', '$http', '$timeout', 'dragularService' , function($scope, $http, $timeout, dragularService ) {
+myDogApp.controller('DogController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+//gets dog at dogs end point in db.json
+  $scope.getDogs = function() {
+    $http.get("http://localhost:4000/mydogs")
+      .then(function(response) {
+        $scope.allDogs = response.data
+      });
+  }
 
-dragularService('.containerVertical');
+  $scope.deleteDog = function(index) {
+    $http.delete("http://localhost:4000/mydogs/" + index)
+       .then(
+           function(response){
+             $scope.getDogs();
+           },
+           function(response){
+             console.log(response)
+             // failure call back
+           }
+        );
 
-$scope.myFunc = function() {
 
-  $http.get("https://dog.ceo/api/breeds/image/random")
-  .then(function(response) {
-    $scope.randomDog = response.data;
-
-    console.log($scope.randomDog)
-  });
-}
-
-$scope.allDogs = [];
-
-$scope.keepDogs = function(allDogs){
-    if ($scope.allDogs.indexOf($scope.randomDog) === -1){
-      $scope.allDogs.push($scope.randomDog);
-      console.log($scope.allDogs)
-    } else {
-      $timeout(function() {
-        $scope.message = "";
-      }, 3000);
-      $scope.message = "This is a duplicate"
-    }
 
   }
 
-  $scope.myName = function(dog){
-    alert("hi my name is"  + " " + dog.name)
+
+//calls api to get random image
+  $scope.myFunc = function() {
+    $http.get("https://dog.ceo/api/breeds/image/random")
+      .then(function(response) {
+        $scope.randomDog = response.data;
+      });
   }
+
+//empty array to hold list of dogs
+  $scope.allDogs = [];
+
+//post to end point of new image and name of dogs
+  $scope.keepDogs = function(allDogs) {
+    $http({
+      method: 'POST',
+      url: 'http://localhost:4000/mydogs',
+      data: {
+        name: $scope.randomDog.name,
+        message: $scope.randomDog.message
+      },
+    }).then(function(response) {
+      $scope.getDogs();
+    })
+  }
+
+//calls dogs list
+  $scope.getDogs();
 
 }]);
