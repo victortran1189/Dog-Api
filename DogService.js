80851b0b97fcca374a371457b481d197beafa9e1
TRAN.VICTOR.CHRISTOPHER.1541469393 <victor.c.tran2.ctr@mail.mil>
2018-04-24 11:26:32 -0400

    css changes

---
+++ DogService.js
@@ -0,0 +1,12 @@
+// myDogApp.service('DogService', function() {
+//
+//
+// this.retrieveDogs = function (response) {
+//   $http.get("https://dog.ceo/api/breeds/image/random")
+//     .then(function(response) {
+//       $scope.randomDog = response.data;
+//     });
+// };
+//
+//
+// });
